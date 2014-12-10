baf110444347aad00db4702c14ab8e707440fbba
csantos0 <ciro.santos@synchro.com.br>
2014-12-10 16:34:11 -0200

    inicial

---
+++ synchro-webapp-quickstart/src/main/java/br/com/synchro/servlet/StartServlet.java
@@ -0,0 +1,19 @@
+package br.com.synchro.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StartServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
+            throws ServletException, IOException {
+        resp.getOutputStream().write("WebApp Servlet started...".getBytes());
+    }
+}
